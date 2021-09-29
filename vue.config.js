e04ad69edbd158e6d81b283f8e7b360995294e31
xiaojuntao <18143945157@163.com>
2021-09-29 19:42:07 +0800

    chore: 修改publicPath

--- vue.config.js
+++ vue.config.js
@@ -6,8 +6,8 @@ console.log('NODE_ENV===', process.env.NODE_ENV);
 console.log('IS_PROD===', IS_PROD);
 
 module.exports = {
-  // publicPath: IS_PROD ? './' : '/',  // 公共路径
-  publicPath: '/',  // 公共路径
+  publicPath: IS_PROD ? '/lily/' : '/',  // 公共路径
+  // publicPath: '/',  // 公共路径
   indexPath: 'index.html', // 相对于打包路径index.html的路径
   outputDir: process.env.outputDir || 'dist', // 'dist', 生产环境构建文件的目录
   assetsDir: 'static', // 相对于outputDir的静态资源(js、css、img、fonts)目录

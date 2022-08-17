ad63417da2b619aa56fe29c39ac442aad1997f49
肖俊涛 <allblue95@126.com>
2022-08-17 09:14:48 +0800

    feat: vxe-tabble使用

--- src/main.js
+++ src/main.js
@@ -16,6 +16,12 @@ import customFn from "./libs/customFn";
 
 import '@/libs/mockEvent.js'
 
+import 'xe-utils'
+import VXETable from 'vxe-table'
+import 'vxe-table/lib/style.css'
+
+Vue.use(VXETable)
+
 // 自定义指令
 import './directive'
 
